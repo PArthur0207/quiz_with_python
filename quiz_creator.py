04f3cc294ffb082d97003df87f9d56505360b4ef
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-07 23:47:18 +0800

    Pseudocode for quiz_creator

---
+++ quiz_creator.py
@@ -0,0 +1,7 @@
+# Use append to add content to the quiz_questions.txt file, creates it if it doesn't exist yet
+# Ask the question
+# Ask for 4 options, make a dictionary for the options
+# Ask for the answer from the options
+# Write the inputs to the quiz_questions.txt file
+# Loop all until the user don't want to input anymore
+# Close the file

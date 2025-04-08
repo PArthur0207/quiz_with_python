85af76ac173ae82b4d7814ef3209ed38f0c08452
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-08 15:49:44 +0800

    Open?create a file using append

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,5 @@
 # Use append to add content to the quiz_questions.txt file, creates it if it doesn't exist yet
+quiz_file = open("quiz_questions.txt", "a")
 # Ask the question
 # Ask for 4 options, make a dictionary for the options
 # Ask for the answer from the options

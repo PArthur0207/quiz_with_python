dbf908e7488b3158a9b3de1f7233be2d86219ad0
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-09 15:16:51 +0800

    Loops the program until the user dont want to input anymore

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,27 +1,32 @@
 # Use append to add content to the quiz_questions.txt file, creates it if it doesn't exist yet
 quiz_file = open("quiz_questions.txt", "a")
 
-# Ask the question
-question = input("Input your question \n>")
+while True: # Loop all until the user don't want to input anymore
+    # Ask the question
+    question = input("Input your question \n>")
 
-# Ask for 4 options, make a dictionary for the options
+    options = {} # Added the dictionary for seperating each letter option
+    # Ask for 4 options, make a dictionary for the options
+    for letter in ['a', 'b', 'c', 'd']:
+        choice = input(f"Input option {letter} \n>")
+        options[letter] = choice
 
-options = {} # Added the dictionary for seperating each letter option
+    # Ask for the answer from the options
+    answer = input("Input your answer here(a/b/c/d) \n>")
 
-for letter in ['a', 'b', 'c', 'd']:
-    choice = input(f"Input option {letter} \n>")
-    options[letter] = choice
+    # Write the inputs to the quiz_questions.txt file
+    quiz_file.write(f"Question: {question} \n")
+    for letter in ['a', 'b', 'c', 'd']:
+        quiz_file.write(f"{letter}.) {options[letter]} \n")
+    quiz_file.write(f"Answer: {answer} \n")
+    quiz_file.write("------------------------ \n")
 
-# Ask for the answer from the options
-answer = input("Input your answer here(a/b/c/d) \n>")
+    again = input("Would you like to input another question? Press 'y' if yes, press any other key if no: ")
+    if again != 'y':
+        print("Thank you for using this quiz creator")
+        break
 
-# Write the inputs to the quiz_questions.txt file
-quiz_file.write(f"Question: {question} \n")
-for letter in ['a', 'b', 'c', 'd']:
-    quiz_file.write(f"{letter}.) {options[letter]} \n")
-quiz_file.write(f"Answer: {answer} \n")
-quiz_file.write("------------------------ \n")
-
-# Loop all until the user don't want to input anymore
 # Close the file
 quiz_file.close()
+
+# Add wow factor (Tkinter inputs)

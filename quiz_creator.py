a4620df35a72599fdc8a9ee16d5c71dfe07437cb
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-09 15:09:29 +0800

    Append the inputs into the txt file

--- quiz_creator.py
+++ quiz_creator.py
@@ -5,13 +5,23 @@ quiz_file = open("quiz_questions.txt", "a")
 question = input("Input your question \n>")
 
 # Ask for 4 options, make a dictionary for the options
+
+options = {} # Added the dictionary for seperating each letter option
+
 for letter in ['a', 'b', 'c', 'd']:
     choice = input(f"Input option {letter} \n>")
+    options[letter] = choice
 
 # Ask for the answer from the options
 answer = input("Input your answer here(a/b/c/d) \n>")
 
 # Write the inputs to the quiz_questions.txt file
+quiz_file.write(f"Question: {question} \n")
+for letter in ['a', 'b', 'c', 'd']:
+    quiz_file.write(f"{letter}.) {options[letter]} \n")
+quiz_file.write(f"Answer: {answer} \n")
+quiz_file.write("------------------------ \n")
+
 # Loop all until the user don't want to input anymore
 # Close the file
 quiz_file.close()

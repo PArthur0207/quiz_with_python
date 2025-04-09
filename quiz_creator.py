29af5004f13925702901b537d5009ea649e0ead0
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-09 14:55:04 +0800

    Ask the user for the inputs, and created the txt file to store the questions

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,17 @@
 # Use append to add content to the quiz_questions.txt file, creates it if it doesn't exist yet
 quiz_file = open("quiz_questions.txt", "a")
+
 # Ask the question
+question = input("Input your question \n>")
+
 # Ask for 4 options, make a dictionary for the options
+for letter in ['a', 'b', 'c', 'd']:
+    choice = input(f"Input option {letter} \n>")
+
 # Ask for the answer from the options
+answer = input("Input your answer here(a/b/c/d) \n>")
+
 # Write the inputs to the quiz_questions.txt file
 # Loop all until the user don't want to input anymore
 # Close the file
+quiz_file.close()

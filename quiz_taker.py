dde07c5d78096ee4c0cc3a9fe6fae35e26808c5a
PArthur0207 <marayanpaularthur1114@gmail.com>
2025-04-30 17:56:01 +0800

    Pseudocode, also firs commit using cellphone

---
+++ quiz_taker.py
@@ -0,0 +1,5 @@
+# Read the quiz questions file
+# Randomly pick a question from the file
+# Do not repeat the questions and end it when it's done
+# Add points when answera are correct and show it in the end
+# Do all this in tkinter

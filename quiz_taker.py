a78827959cf5058f74bcb58426dc745db03dfaad
PArthur0207 <marayanpaularthur1114@gmail.com>
2025-05-01 10:56:28 +0800

    Read the questions from the txt file

--- quiz_taker.py
+++ quiz_taker.py
@@ -1,5 +1,12 @@
 # Read the quiz questions file
+with open("quiz_questions.txt", "r") as quiz_file:
+    reader = quiz_file.readlines()
+    
+for lines in reader:
+    print(lines)
+    
 # Randomly pick a question from the file
 # Do not repeat the questions and end it when it's done
 # Add points when answera are correct and show it in the end
 # Do all this in tkinter
+ 
